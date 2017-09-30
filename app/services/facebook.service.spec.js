a21bb6b6d951bf5b00d1587a823c809f24494210
SlimDogs <SlimDogs@gmail.com>
2017-09-30 21:09:19 +0100

    CI and unit tests initial setup

---
+++ app/services/facebook.service.spec.js
@@ -0,0 +1,7 @@
+const ut = require('unit.js');
+
+describe('Facebook service tests', () => {
+    it('Should post a message and fire the callback', () => {
+        ut.must(true).be(true);
+    });
+});
