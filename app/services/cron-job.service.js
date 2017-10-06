0bffccfe602a90948d020b59c008bdf450031641
SlimDogs <SlimDogs@gmail.com>
2017-10-06 20:26:15 +0100

    feat(Cron): Added Cron Job service allowing to run certain tasks on selected times
    Added Cron Job service which at this point is performing names day scrapping and posting task at
    selected times

---
+++ app/services/cron-job.service.js
@@ -0,0 +1,72 @@
+const moment = require('moment-timezone')
+
+class CronJobTask {
+  constructor (cronJob, cronJobService) {
+    this.cronJob = cronJob
+    this.cronJobService = cronJobService
+  }
+
+  start () {
+    this.cronJobService._startChronJob(this.cronJob)
+  }
+
+  stop () {
+    this.cronJobService._stopChronJob(this.cronJob)
+  }
+}
+
+class CronJobService {
+  constructor () {
+    if (!global.WSCF_CHRON_JOBS) {
+      global.WSCF_CHRON_JOBS = {
+        available: [],
+        running: []
+      }
+
+      setInterval(this.checkChronJobs.bind(this), 60000)
+    }
+  }
+
+  addChronJob (cronJobDescription) {
+    const newChronJobTask = new CronJobTask(cronJobDescription, this)
+    global.WSCF_CHRON_JOBS.available.push(newChronJobTask)
+
+    return newChronJobTask
+  }
+
+  checkChronJobs () {
+    console.log('Performing chron job check')
+    const currentDate = new Date()
+    global.WSCF_CHRON_JOBS.running.forEach(task => {
+      const taskTimezoneDate = task.timezone ? moment.tz(currentDate, task.timezone) : moment.tz(currentDate)
+      const currentTime = taskTimezoneDate.format('HH:mm')
+
+      if (task.days.indexOf(parseInt(taskTimezoneDate.day()) >= 0)) {
+        try {
+          task.times.forEach(time => {
+            if (time === currentTime) {
+              task.job()
+              throw new Error()
+            }
+          })
+        } catch (error) {
+          console.log(`${currentTime} - ${task.description} (Cron job executed)`)
+          process.stdin.resume()
+        }
+      }
+    })
+  }
+
+  _startChronJob (chronJobTask) {
+    global.WSCF_CHRON_JOBS.running.push(chronJobTask)
+  }
+
+  _stopChronJob (chronJobTask) {
+    const chronJobTaskIndex = global.WSCF_CHRON_JOBS.running.indexOf(chronJobTask)
+    if (chronJobTaskIndex >= 0) {
+      global.WSCF_CHRON_JOBS.running.splice(chronJobTaskIndex, 1)
+    }
+  }
+}
+
+module.exports = new CronJobService()
--- server.js
+++ server.js
@@ -8,10 +8,18 @@ const constants = require('./app/constants.js')
 const facebook = require('./app/services/facebook.service.js')
 const formatter = require('./app/services/formatter.service.js')
 const graphicsService = require('./app/services/graphics.service.js')
+const cronJobService = require('./app/services/cron-job.service.js')
 
 class WebScrapperFacebookPoster {
   constructor () {
-    this.scrap()
+    const namesDayCronJob = cronJobService.addChronJob({
+      description: 'Names day posting to Lithuanian audience Facebook pages',
+      timezone: 'Europe/Riga',
+      days: [0, 1, 2, 3, 4, 5, 6],
+      times: ['12:00'],
+      job: this.scrap.bind(this)
+    })
+    namesDayCronJob.start()
   }
 
   scrap () {
