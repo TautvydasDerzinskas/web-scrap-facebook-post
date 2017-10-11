e88486c18eb5ce4f336847d5d8bd74423f5062cf
Tautvydas Derzinskas <t.derzinskas@quidco.com>
2017-10-11 14:37:12 +0100

    test(Facebook service): Unit tests for Facebook service methods

--- app/services/facebook.service.spec.js
+++ app/services/facebook.service.spec.js
@@ -1,7 +1,35 @@
 const ut = require('unit.js')
+const mock = require('mock-require')
+
+mock('https', {
+  request: function (config, callback) {
+    // eslint-disable-next-line
+    callback('Image posted')
+    return { on: () => {} }
+  }
+})
+mock('request', function (config, callback) { callback(null, 'Message posted', {}) })
+mock('fs', { createReadStream: function () { } })
+mock('form-data', function () {
+  this.append = function () {}
+  this.pipe = function () {}
+  this.getHeaders = function () {}
+})
+
+const facebookService = require('./facebook.service.js')
 
 describe('Facebook service tests', () => {
-  it('Should post a message and fire the callback', () => {
-    ut.must(true).be(true)
+  it('Should post a message', (done) => {
+    facebookService.postMessage('Sample message').then(resp => {
+      ut.must(resp).be('Message posted')
+      done()
+    })
+  })
+
+  it('Should post a image', (done) => {
+    facebookService.postImage('Sample message', 'app/image/test.png').then(resp => {
+      ut.must(resp).be('Image posted')
+      done()
+    })
   })
 })
